8f9ed691304dfa39d72b55188b99e2d257702a29
Alejandro <alexgago99@gmail.com>
2022-12-09 20:49:47 +0100

    modificar

--- area_admin.php
+++ area_admin.php
@@ -47,6 +47,7 @@
         }
     }
         ?>
+        <br>
             <div class="                           
                     ">
                 <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
